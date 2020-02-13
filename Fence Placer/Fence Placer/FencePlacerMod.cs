2a495f3a8edefbde58558979ccfb0272b21a616e
CalvinDong <48747266+CalvinDong@users.noreply.github.com>
2020-02-14 00:30:58 +1100

    Add files via upload

---
+++ Fence Placer/Fence Placer/FencePlacerMod.cs
@@ -0,0 +1,20 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace Tutorial
+{
+
+    public class FencePlacerMod : IUserMod
+    {
+
+        public string Name
+        {
+            get { return "Fence Placer Mod"; }
+        }
+
+        public string Description
+        {
+            get { return "Automatically places fences down in a selected area"; }
+        }
+    }
+}
